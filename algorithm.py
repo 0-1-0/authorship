c75925334f5e579c18d99a207cb505b2c0270ccd
Nickolay Yegorov <nickolay.yegorov@gmail.com>
2015-09-22 14:22:55 +0300

    improved accuracy by introducing new features

--- algorithm.py
+++ algorithm.py
@@ -18,35 +18,75 @@ from spacy import parts_of_speech
 from spacy.en import English
 from nltk.corpus import stopwords
 
-cachedStopWords = stopwords.words("english")
+cachedStopWords = set(stopwords.words("english"))
+cachedPunctuation = set(list(string.punctuation))
 nlp = English()
+
+docCache = {}
+
+
+# parsing is time-consuming operation, so we perform caching
+def get_doc(x):
+    if x not in docCache:
+        docCache[x] = nlp(unicode(x))
+    return docCache[x]
 
 
 def extract_pos(x):
-    doc = nlp(unicode(x))
+    doc = get_doc(x)
     tokens = [token.pos for token in doc]
     # print res
     return tokens
 
-table = string.maketrans("", "")
-
 
 def extract_pun(x):
-    return [c for c in x if c in string.punctuation]
+    doc = get_doc(x)
+    return [t.lemma for t in doc if t.is_punct]
 
 
 def named_entity(x):
-    doc = nlp(unicode(x))
-    return [ent.label_ for ent in doc.ents]
+    doc = get_doc(x)
+    return [ent.label for ent in doc.ents]
 
 
 def word_per_sent(x):
-    doc = nlp(unicode(x))
-    return ([len([w for w in s]) for s in doc.sents])
+    doc = get_doc(x)
+    return [len(s) for s in doc.sents]
 
 
-def extract_stop_words(x):
-    return [w for w in x.split() if x in stopwords.words("english")]
+def stop_word_per_sent(x):
+    doc = get_doc(x)
+    return [len([w for w in s if w.lemma_ in cachedStopWords]) for s in doc.sents]
+
+
+def numbers_cnt(x):
+    doc = get_doc(x)
+    return [t.like_num for t in doc]
+
+
+def capitals_cnt(x):
+    doc = get_doc(x)
+    return [t.is_title for t in doc]
+
+
+def get_prefixes(x):
+    doc = get_doc(x)
+    return [w.prefix_ for w in doc]
+
+
+def get_suffixes(x):
+    doc = get_doc(x)
+    return [w.suffix_ for w in doc]
+
+
+def get_lemmas(x):
+    doc = get_doc(x)
+    return [w.lemma for w in doc]
+
+
+def get_clusters(x):
+    doc = get_doc(x)
+    return [t.cluster for t in doc]
 
 
 class Model(BaseEstimator, ClassifierMixin):
@@ -55,28 +95,32 @@ class Model(BaseEstimator, ClassifierMixin):
         self.classes_ = None
         self.sel = None
         self.vectorizer_params = [
-            {'analyzer': "word", 'ngram_range': (2, 3), 'binary': False, 'max_features': 2000}, # word frequencies
-            {'analyzer': "char", 'ngram_range': (2, 3), 'binary': False, 'max_features': 2000, 'min_df': 0}, # character freqs.
-            {'analyzer': extract_pos, 'ngram_range': (2, 4), 'binary': False, 'max_features': 2000, 'min_df': 0}, # POS freqs.
-            # {'analyzer': extract_pun, 'ngram_range': (1, 1), 'binary': False, 'max_features': 2000, 'min_df': 0}, # Punct. freqs.
-            {'analyzer': named_entity, 'ngram_range': (1, 1), 'binary': False, 'max_features': 200, 'min_df': 0}, # NE. freqs.
+            {'analyzer': "word", 'ngram_range': (1, 3), 'binary': False, 'max_features': 2000}, # word frequencies
+            # {'analyzer': "char", 'ngram_range': (2, 3), 'binary': False, 'max_features': 2000, 'min_df': 0}, # character freqs.
+            {'analyzer': extract_pos, 'ngram_range': (1, 4), 'binary': False, 'max_features': 2000, 'min_df': 0}, # POS freqs.
+            {'analyzer': extract_pun, 'ngram_range': (1, 2), 'binary': False, 'max_features': 2000, 'min_df': 0}, # Punct. freqs.
+            {'analyzer': named_entity, 'ngram_range': (1, 2), 'binary': False, 'max_features': 200, 'min_df': 0}, # NE. freqs.
+
+            {'analyzer': numbers_cnt, 'ngram_range': (1, 1), 'binary': False, 'max_features': 10, 'min_df': 0}, # WPS. freqs.
+            {'analyzer': capitals_cnt, 'ngram_range': (1, 1), 'binary': False, 'max_features': 10, 'min_df': 0}, # WPS. freqs.
+
+            {'analyzer': get_prefixes, 'ngram_range': (1, 1), 'binary': False, 'max_features': 200, 'min_df': 0}, # WPS. freqs.
+            {'analyzer': get_suffixes, 'ngram_range': (1, 1), 'binary': False, 'max_features': 200, 'min_df': 0}, # WPS. freqs.
+            {'analyzer': get_lemmas, 'ngram_range': (1, 1), 'binary': False, 'max_features': 500, 'min_df': 0}, # WPS. freqs.
+            {'analyzer': get_clusters, 'ngram_range': (1, 2), 'binary': False, 'max_features': 2000, 'min_df': 0}, # WPS. freqs.
+
             {'analyzer': word_per_sent, 'ngram_range': (1, 1), 'binary': False, 'max_features': 200, 'min_df': 0}, # WPS. freqs.
-            # {'analyzer': extract_stop_words, 'ngram_range': (1, 2), 'binary': False, 'max_features': 2000, 'min_df': 0} # stop words. freqs.
+            {'analyzer': stop_word_per_sent, 'ngram_range': (1, 2), 'binary': False, 'max_features': 200, 'min_df': 0} # stop words. freqs.
         ]
 
     def vectorize(self, X):
-        # remove punctuation
-        X1 = []
-        for x in X:
-            X1.append(x.translate(table, string.punctuation))
-
         vectorizers = [TfidfVectorizer() for _ in self.vectorizer_params]
         for (params, v) in zip(self.vectorizer_params, vectorizers):
             v.set_params(**params)
 
         vectorizers = [(str(i), v) for i, v in enumerate(vectorizers)]
         vectorizer = FeatureUnion(vectorizers)
-        matrix = vectorizer.fit_transform(X1)
+        matrix = vectorizer.fit_transform(X)
         XX = matrix.toarray()
 
         for i, v in enumerate(vectorizers):
@@ -88,7 +132,7 @@ class Model(BaseEstimator, ClassifierMixin):
         print 'training model..'
 
         XX = self.vectorize(X)
-        self.sel = SelectKBest(chi2, k=4000)
+        self.sel = SelectKBest(chi2, k='all')
         XX = self.sel.fit_transform(XX, y)
 
         self.cls = LogisticRegression()
